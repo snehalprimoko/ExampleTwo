f262ba99368841dc20cf243ef5de8464d37f9452
Xcode Server <xcodeserver@Primokos-MacBook-Air.local>
2017-03-20 20:41:56 -0400

    ic

---
+++ Pods/Target Support Files/Pods-exampletwo/Pods-exampletwo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_exampletwo : NSObject
+@end
+@implementation PodsDummy_Pods_exampletwo
+@end
---
+++ Pods/Target Support Files/Pods-exampletwo/Pods-exampletwo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_exampletwoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_exampletwoVersionString[];
+
